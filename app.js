a054da24bf6ca07ef320aa95fa2b338029c48579
ClarkGH <clarkhinchcliff@hotmail.com>
2014-11-29 01:35:44 -0700

    created app.js -added license -added README

--- app.js
+++ app.js
@@ -1,45 +1,21 @@
-var http = require('http');
-  fs = require('fs');
+var express = require('express');
 
-function serveStaticFile(res, path, contentType, responseCode) {
-  if(!responseCode) responseCode = 200;
-  fs.readFile(__dirname + path, function(err, data) {
-    if(err) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('500 - Internal Error');
-    } else {
-      res.writeHead(responseCode,
-        { 'Content-Type': contentType });
-      res.end(data);
-    }
-  });
-}
+var app = express();
 
-http.createServer(function(req,res){
-  // normalize url by removing querystring, optional
-  // trailing slash, and making it lowercase
-  var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+app.set('port', process.env.PORT || 3000);
 
-  switch(path) {
+app.use(function(req, res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
 
-    case '':
-      serveStaticFile(res, '/public/home.html', 'text/html');
-      break;
+app.use(function(req, res){
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 - Internal Error');
+});
 
-    case '/about':
-      serveStaticFile(res, '/public/about.html', 'text/html');
-      break;
-
-    case '/img/logo.png':
-      serveStaticFile(res, '/public/img/logo.png', 'image/png');
-      break;
-
-    default:
-      serveStaticFile(res, '/public/404.html', 'text/html', 404);
-      break;
-
-  }
-
-}).listen(3000);
-
-console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
+app.listen(app.get('port'), function(){
+  console.log( 'The express application is running on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
+});

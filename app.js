25a7c8d03787807dc06489af1c883017cd0e78c3
ClarkGH <clarkhinchcliff@hotmail.com>
2014-11-29 13:42:58 -0700

    added get routes for home and about pages

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+app.get('/', function(req, res){
+  res.type('text/plain');
+  res.send('Main Page');
+});
+
+app.get('/about', function(req, res){
+  res.type('text/plain');
+  res.send('About Page');
+});
+
 app.use(function(req, res){
   res.type('text/plain');
   res.status(404);

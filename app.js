f269afc636b0971598220b080c15f5f2999d2779
ClarkGH <clarkhinchcliff@hotmail.com>
2014-11-29 15:32:46 -0700

    added home layout to route -page isn't loading

--- app.js
+++ app.js
@@ -4,28 +4,27 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function(req, res){
-  res.type('text/plain');
-  res.send('Main Page');
+app.get('/', function(req, res) {
+  res.render('home');
 });
 
-app.get('/about', function(req, res){
+app.get('/about', function(req, res) {
   res.type('text/plain');
   res.send('About Page');
 });
 
-app.use(function(req, res){
+app.use(function(req, res) {
   res.type('text/plain');
   res.status(404);
   res.send('404 - Not Found');
 });
 
-app.use(function(req, res){
+app.use(function(req, res) {
   res.type('text/plain');
   res.status(500);
   res.send('500 - Internal Error');
 });
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function() {
   console.log( 'The express application is running on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
 });

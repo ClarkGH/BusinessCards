b2184b70b01fef2331b6f8b5d060adc41e0d6442
ClarkGH <clarkhinchcliff@hotmail.com>
2014-11-30 00:03:35 -0700

    test still not passing in mocha

--- qa/tests-global.js
+++ qa/tests-global.js
@@ -1,1 +1,5 @@
-//recreate test
+suite('Global Tests', function(){
+  test('page has a valid title', function(){
+    assert(document.title && document.title.match(/\S/) && document.title.toUpperCase() !== 'TODO');
+  });
+});

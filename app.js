7c9eae3bca620be3b9093d08910539f2ca0d0ecd
ClarkGH <clarkhinchcliff@hotmail.com>
2014-12-02 15:46:39 -0700

    added some models -readded mongoose and body parser support -made some of the code more readable

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-var express = require('express');
-var app = express();
+var express         = require('express');
+var app             = express();
 var adviceContainer = require('./lib/advice.js');
-var main = require('./handlers/main.js');
+var main            = require('./handlers/main.js');
+var bodyParser      = require('body-parser')
+var mongoose        = require('mongoose')
+var credentials     = require('./credentials.js')
 
 var handlebars = require('express3-handlebars')
   .create({ defaultLayout:'main' });
 
+//bodyParser() config to allow getting POST data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+//Port setting
+app.set('port', process.env.PORT || 3000);
+
+//Handlebars
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-app.set('port', process.env.PORT || 3000);
+//Databases
+mongoose.connect(credentials.mongo.development.connectionString);
+
 
 //Middleware
 app.use(express.static(__dirname + '/public'));
@@ -23,6 +35,7 @@ app.use(function(req, res, next){
 //Routes
 require('./routes.js')(app);
 
+//Errors
 app.use(function(req, res) {
   res.status(404);
   res.render('404');
@@ -33,6 +46,7 @@ app.use(function(req, res) {
   res.render('500');
 });
 
+//Listener
 app.listen(app.get('port'), function() {
   console.log( 'The express application is running on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
 });

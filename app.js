5f021308f1a3c4c4c4be33c02160e824365a8b43
ClarkGH <clarkhinchcliff@hotmail.com>
2014-12-02 14:10:08 -0700

    Modularized routes

--- app.js
+++ app.js
@@ -20,26 +20,8 @@ app.use(function(req, res, next){
 });
 
 //Routes
-app.get('/', function(req, res) {
-  res.render('home');
-});
+require('./routes.js')(app);
 
-app.get('/about', function(req, res) {
-  res.render('about', {
-    advice: adviceContainer.getAdvice(),
-    pageTestScript: '/qa/tests-about.js'
-  } );
-});
-
-app.use(function(req, res) {
-  res.status(404);
-  res.render('404');
-});
-
-app.use(function(req, res) {
-  res.status(500);
-  res.render('500');
-});
 
 app.listen(app.get('port'), function() {
   console.log( 'The express application is running on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
---
+++ routes.js
@@ -0,0 +1,26 @@
+var main = require('./handlers/main.js')
+
+module.exports = function(app){
+
+  app.get('/', function(req, res) {
+    res.render('home');
+  });
+
+  app.get('/about', function(req, res) {
+    res.render('about', {
+      advice: adviceContainer.getAdvice(),
+      pageTestScript: '/qa/tests-about.js'
+    } );
+  });
+
+  app.use(function(req, res) {
+    res.status(404);
+    res.render('404');
+  });
+
+  app.use(function(req, res) {
+    res.status(500);
+    res.render('500');
+  });
+
+};

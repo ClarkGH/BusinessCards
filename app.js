190c265a1bfd79cd8fc93ce5a0649887b3c60717
ClarkGH <clarkhinchcliff@hotmail.com>
2014-12-02 07:27:06 -0700

    added routes and server connection

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 var adviceContainer = require('./lib/advice.js');
 var credentials = require('./credentials.js');
 
@@ -12,6 +13,23 @@ var handlebars = require('express3-handlebars').create({ defaultLayout:'main',
     }
   }
 });
+
+var opts = {
+  server: {
+    socketOptions: { keepAlive: 1 }
+  }
+};
+
+switch(app.get('env')){
+  case 'development':
+  mongoose.connect(credentials.mongo.development.connectionString, opts);
+  break;
+  case 'production':
+  mongoose.connect(credentials.mongo.production.connectionString, opts);
+  break
+  default:
+    throw new Error('Unknown execution environment: ' + app.get('env'));
+}
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');

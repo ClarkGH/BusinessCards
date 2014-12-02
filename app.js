da822f83cc9ee3a8d65159305fd2106cdc2ee428
ClarkGH <clarkhinchcliff@hotmail.com>
2014-12-02 10:28:29 -0700

    Server no longer works. Restarting application.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 var adviceContainer = require('./lib/advice.js');
-var credentials = require('./credentials.js');
-var Test = require('./models/test.js');
+// var credentials = require('./credentials.js');
+// var Test = require('./models/test.js');
 
 var handlebars = require('express3-handlebars').create({ defaultLayout:'main',
   helpers: {
@@ -15,11 +15,11 @@ var handlebars = require('express3-handlebars').create({ defaultLayout:'main',
   }
 });
 
-var opts = {
-  server: {
-    socketOptions: { keepAlive: 1 }
-  }
-};
+// var opts = {
+//   server: {
+//     socketOptions: { keepAlive: 1 }
+//   }
+// };
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -28,29 +28,29 @@ app.set('view engine', 'handlebars');
 app.disable('x-powered-by');
 
 // DB connection
-switch(app.get('env')){
-  case 'development':
-    mongoose.connect(credentials.mongo.development.connectionString, opts);
-    break;
-  case 'production':
-    mongoose.connect(credentials.mongo.development.connectionString, opts);
-    break;
-  default:
-    throw new Error('Unknown execution environment: ' + app.get('env'));
-}
+// switch(app.get('env')){
+//   case 'development':
+//     mongoose.connect(credentials.mongo.development.connectionString, opts);
+//     break;
+//   case 'production':
+//     mongoose.connect(credentials.mongo.development.connectionString, opts);
+//     break;
+//   default:
+//     throw new Error('Unknown execution environment: ' + app.get('env'));
+// }
 
 //Seed data
-Test.find(function(err, test){
-  if(test.length) return;
+// Test.find(function(err, test){
+//   if(test.length) return;
 
-  new Test({
-    name: 'Bill',
-    description: 'I like pudding',
-    available: true,
-    inSeason: true,
-    age: 22,
-  }).save();
-});
+//   new Test({
+//     name: 'Bill',
+//     description: 'I like pudding',
+//     available: true,
+//     inSeason: true,
+//     age: 22,
+//   }).save();
+// });
 
 //Set port
 app.set('port', process.env.PORT || 3000);
@@ -92,38 +92,38 @@ app.get('/author/contact-clark', function(req, res){
   res.render('author/contact-clark');
 });
 
-//delete later, an example
-app.get('/newsletter', function(req, res){
-  res.render('newsletter', { csrf: 'CSRF token goes here.'});
-});
+// //delete later, an example
+// app.get('/newsletter', function(req, res){
+//   res.render('newsletter', { csrf: 'CSRF token goes here.'});
+// });
 
-//delete later, an example
-app.post('/process', function(req, res){
-  if(req.xhr || req.accepts('json,html')==='json'){
-    res.send({ success: true });
-  } else {
-    res.redirect(303, '/');
-  }
-});
+// //delete later, an example
+// app.post('/process', function(req, res){
+//   if(req.xhr || req.accepts('json,html')==='json'){
+//     res.send({ success: true });
+//   } else {
+//     res.redirect(303, '/');
+//   }
+// });
 
-//delete later, route for test
+// //delete later, route for test
 
-app.get('/test', function(req, res){
-  Test.find({ available: true }, function(err, test){
-    var context = {
-      test: test.map(function(test){
-        return {
-          name: test.name,
-          description: test.description,
-          available: test.available,
-          inSeason: test.inSeason,
-          age: test.age,
-        }
-      })
-    };
-    res.render('test', context);
-  });
-});
+// app.get('/test', function(req, res){
+//   Test.find({ available: true }, function(err, test){
+//     var context = {
+//       test: test.map(function(test){
+//         return {
+//           name: test.name,
+//           description: test.description,
+//           available: test.available,
+//           inSeason: test.inSeason,
+//           age: test.age,
+//         }
+//       })
+//     };
+//     res.render('test', context);
+//   });
+// });
 
 //Run
 app.listen(app.get('port'), function() {
